1d5561078f0625a0d48e3491c5c1f845be120a24
Ahmeeedd <60976715+Ahmeeedd@users.noreply.github.com>
2021-04-14 03:19:25 +0200

    first commit

--- KinderGarten/Service/Utils/Statics.cs
+++ KinderGarten/Service/Utils/Statics.cs
@@ -10,7 +10,7 @@ namespace Service
     {
 
         public static string baseAddress = "http://localhost:8081/";
-
-        public static string _AccessToken = " eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0ZG9jdG9yQHVzZXIuY29tIiwiYXV0aCI6IlJPTEVfZG9jdG9yIiwiaWF0IjoxNjE4MzAzODgxLCJleHAiOjE2MTkxNjc4ODF9.py_g0ElX7vsbfz9BROQ4DQJPJ862CRgf9kYCQrOdhvg";
+       // public static string _AccessToken = " eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0ZG9jdG9yQHVzZXIuY29tIiwiYXV0aCI6IlJPTEVfZG9jdG9yIiwiaWF0IjoxNjE4MzAzODgxLCJleHAiOjE2MTkxNjc4ODF9.py_g0ElX7vsbfz9BROQ4DQJPJ862CRgf9kYCQrOdhvg";
+       public static string _AccessToken = " eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0YWRtaW5nYXJ0ZW5AdXNlci5jb20iLCJhdXRoIjoiUk9MRV9hZG1pbkdhcnRlbiIsImlhdCI6MTYxODM1ODQwNiwiZXhwIjoxNjE5MjIyNDA2fQ.6Pqt5gj51AAKfhlS1gzFMBTPe3U3_wrWCQV0Js-YUxA";
     }
 }
