caccfb1c2492482538a4d8ea6c71c281d99bb0dc
Ahmeeedd <60976715+Ahmeeedd@users.noreply.github.com>
2021-04-20 05:43:19 +0200

    correction commit

--- KinderGarten/Service/Utils/Statics.cs
+++ KinderGarten/Service/Utils/Statics.cs
@@ -11,10 +11,7 @@ namespace Service
 
         public static string baseAddress = "http://localhost:8081/";
 
-<<<<<<< Updated upstream
-        public static string _AccessToken = " eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0ZG9jdG9yQHVzZXIuY29tIiwiYXV0aCI6IlJPTEVfZG9jdG9yIiwiaWF0IjoxNjE4ODM3NDk5LCJleHAiOjE2MTk3MDE0OTl9.d6FHpwUGFJAn7glv6RBXDrCKilixqlvYnsyd2UpA8Vw";
-=======
         public static string _AccessToken = " eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0YWRtaW5nYXJ0ZW5AdXNlci5jb20iLCJhdXRoIjoiUk9MRV9hZG1pbkdhcnRlbiIsImlhdCI6MTYxODYxMjIzMSwiZXhwIjoxNjE5NDc2MjMxfQ.ikadnIaTyCQBP9LHQoj2U871WiGTFqumWvLNAasGNKQ";
->>>>>>> Stashed changes
+
     }
 }
