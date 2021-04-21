4360748caafba501f0d9788649ad37e3ba1b9bff
ZOUARIoussema <60824835+ZOUARIoussema@users.noreply.github.com>
2021-04-21 02:06:46 +0100

    accounting

--- KinderGarten/Service/Utils/Statics.cs
+++ KinderGarten/Service/Utils/Statics.cs
@@ -11,7 +11,7 @@ namespace Service
 
         public static string baseAddress = "http://localhost:8081/";
 
-        public static string _AccessToken = " eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0YWdlbnRjYXNoaWVyQHVzZXIuY29tIiwiYXV0aCI6IlJPTEVfYWdlbnRDYXNoaWVyIiwiaWF0IjoxNjE4OTE5MzIzLCJleHAiOjE2MTk3ODMzMjN9.WXa7laNzJ28WEmSFK1Ge08wzu3ziyybosBSI4_m0a10";
+        public static string _AccessToken = " eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0YWdlbnRjYXNoaWVyQHVzZXIuY29tIiwiYXV0aCI6IlJPTEVfYWdlbnRDYXNoaWVyIiwiaWF0IjoxNjE4OTYzOTI2LCJleHAiOjE2MTk4Mjc5MjZ9.tSVj44nVf4sx7fp7JbplALXgAPtebN4VC8EWjSsfgfA";
 
     }
 }
---
+++ KinderGarten/Web/Controllers/AccountingController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class AccountingController : Controller
+    {
+        // GET: Accounting
+        public ActionResult Index()
+        {
+           ViewBag.LienXL = "http://localhost:8081/accounting/export/excel/"+DateTime.Now.ToString("d");
+            return View();
+        }
+    }
+}
